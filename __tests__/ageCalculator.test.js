b43072437b7ac97f89f5287d3bf4e2a1634d82c2
Ryan Walker <ryandanielwalker@gmail.com>
2021-04-16 09:28:39 -0700

    Refactor ageCalculator constructor to include lifeExpectancy property

--- __tests__/ageCalculator.test.js
+++ __tests__/ageCalculator.test.js
@@ -1,15 +1,16 @@
 import AgeCalculator from '../src/js/ageCalculator.js'
 
 describe('AgeCalculator', () => {
-  test('should create an object with two properties: planetName: Earth, userAge: 32', () => {
-    const newCalculator = new AgeCalculator("earth", 32)
+  test('should create an object with two properties: planetName: Earth, userAge: 32, lifeExpectancy: 77', () => {
+    const newCalculator = new AgeCalculator("earth", 32, 77)
     expect(newCalculator.planetName).toEqual("earth");
     expect(newCalculator.userAge).toEqual(32)
+    expect(newCalculator.lifeExpectancy).toEqual(77)
   });
 
-  test('should calculate users age on Mercury based on inputted age', () => {
-    let newUser = new AgeCalculator("mercury", 32)
-    expect(newUser.mercuryCalc()).toEqual(Math.round(32 * .24))
-  })
+  // test('should calculate users age on Mercury based on inputted age', () => {
+  //   let newUser = new AgeCalculator("mercury", 32)
+  //   expect(newUser.mercuryCalc()).toEqual(Math.round(32 * .24))
+  // })
 })
 
--- src/js/ageCalculator.js
+++ src/js/ageCalculator.js
@@ -1,12 +1,14 @@
 export default class AgeCalculator {
-  constructor(planetName, userAge) {
+  constructor(planetName, userAge, lifeExpectancy) {
     this.planetName = planetName;
     this.userAge = userAge;
+    this.lifeExpectancy = lifeExpectancy;
   }
 
-  mercuryCalc() {
-
-  }
+  // mercuryCalc() {
+  //   this.userAge = Math.round(this.userAge * .24)
+  //   return this.userAge
+  // }
 
 
 
