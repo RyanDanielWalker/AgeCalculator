eb2235e0d93836c88991bf3bad31ead8ef8f99e8
Ryan Walker <ryandanielwalker@gmail.com>
2021-04-16 16:17:30 -0700

    WIP: continue work on UI logic and styling

--- src/main.js
+++ src/main.js
@@ -4,23 +4,37 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import AgeCalculator from '../src/js/ageCalculator.js';
 
-$(document).ready(() => {
-  $("#planetForm").submit((event) => {
-    event.preventDefault();
-    let planetName = $("#planetChoice").val();
-    let userAge = $("#userAge").val()
-    let lifeExpectancy = $("#lifeExpectancy").val()
-    let newUser = new AgeCalculator(planetName, userAge, lifeExpectancy);
-    if (newUser.planetName === "Mercury") {
-      newUser.mercuryCalc();
-    } else if (newUser.planetName === "Venus") {
-      newUser.venusCalc();
-    } else if (newUser.planetName === "Mars") {
-      newUser.marsCalc();
-    } else if (newUser.planetName === "Jupiter") {
-      newUser.jupiterCalc();
-    }
+// $(document).ready(() => {
+$("#planetForm").submit((event) => {
+  event.preventDefault();
+  let planetName = $("#planetChoice").val();
+  let userAge = $("#userAge").val();
+  let lifeExpectancy = $("#lifeExpectancy").val();
+  let newUser = new AgeCalculator(planetName, userAge, lifeExpectancy);
+  console.log(newUser);
+  if (newUser.planetName === "Mercury") {
+    newUser.mercuryCalc();
     newUser.yearsLeftCalc();
-  })
-
+    $("#output").text(newUser.lifeExpectancy);
+  } else if (newUser.planetName === "Venus") {
+    newUser.venusCalc();
+    newUser.yearsLeftCalc();
+    $("#output").text(newUser.lifeExpectancy);
+  } else if (newUser.planetName === "Mars") {
+    newUser.marsCalc();
+    newUser.yearsLeftCalc();
+    $("#output").text(newUser.lifeExpectancy);
+  } else if (newUser.planetName === "Jupiter") {
+    newUser.jupiterCalc();
+    newUser.yearsLeftCalc();
+    $("#output").text(newUser.lifeExpectancy);
+  }
 });
+// });
+
+
+
+
+
+
+
