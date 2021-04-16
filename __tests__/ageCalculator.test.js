953896f430de24be66f0dfa49a4579115e9dda3e
Ryan Walker <ryandanielwalker@gmail.com>
2021-04-16 09:56:27 -0700

    Good fail test for mercuryCalc lifeExpectancy

--- __tests__/ageCalculator.test.js
+++ __tests__/ageCalculator.test.js
@@ -12,8 +12,9 @@ describe('AgeCalculator', () => {
     let newUser = new AgeCalculator("mercury", 32)
     expect(newUser.mercuryCalc()).toEqual(133)
   });
-  // test('should calculate user life expectancy based on user age on mercury', {
-
-  // })
+  test('should calculate user life expectancy based on user age on mercury', () => {
+    let newUser = new AgeCalculator("mercury", 32, 77)
+    expect(newUser.mercuryCalc()).toEqual(321)
+  })
 })
 
